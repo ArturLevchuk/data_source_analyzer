05ca5080ba017d7ec09ab4066dd64c50ad16609f
ArturLevchuk <levchukarturr@gmail.com>
2024-02-21 16:01:41 +0200

    fixes for realease ver

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -6,7 +6,7 @@ import 'package:flutter/material.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await initDataSourceAnalyzerLib();
+  initDataSourceAnalyzerLib();
   runApp(const MyApp());
 }
 
